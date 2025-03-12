7ead51d7225e0144f24ee11879019b8ec789fab8
Heeseon1215 <skgmltjs01@naver.com>
2025-03-12 15:31:54 +0900

    0312

--- custom.js
+++
@@ -1,9 +0,0 @@
-setTimeout(() => {
-      let iframe = document.querySelector("iframe");
-      if (iframe) {
-        iframe.contentWindow.postMessage(
-          '{"event":"command","func":"mute","args":""}',
-          "*"
-        );
-      }
-    }, 2000);
